fc19b6575029c5621c8a2175d9bae61ec24d3397
周桂 <602518578@qq.com@d447c8be-362a-4498-ba14-30221662a69d>
2018-12-22 15:38:03 +0000

    spring boot 集成mysql、mybatias

---
+++ chapter3_2/src/main/java/com/springcloudlearning/chapter3_2/Chapter32Application.java
@@ -0,0 +1,14 @@
+package com.springcloudlearning.chapter3_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter32Application.class, args);
+	}
+
+}
+
